a373a5173e60378c803529bda6ca7720af8b127a
Pavel Tarakanov <pavel191094@icloud.com>
2025-09-10 12:49:31 +0300

    simple server, return html

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const http = require('http');
 
 http.createServer((req, res) => {
+    console.log(req.url)
+    console.log(req.method)
     console.log("Server is running");
-    res.end("1")
+    res.setHeader("Content-Type","text/html; charset=utf-8;");
+    res.write("<h1>Hello Pattaya!</h1>");
+    res.write("<p>I am learning Node.js!</p>");
+    res.end()
 }).listen(3500);
